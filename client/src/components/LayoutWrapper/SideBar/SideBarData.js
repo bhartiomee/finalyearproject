44ac1b5dded421d057eac27b5a98c79c59a0b1cf
bhartiomee <bhartiomee25@gmail.com>
2022-06-12 12:05:30 +0530

    Initial changes and setup

---
+++ client/src/components/LayoutWrapper/SideBar/SideBarData.js
@@ -0,0 +1,17 @@
+import { ReactComponent as GlobalIcon } from '../../../assets/Globe.svg';
+
+export const SideBarData = [
+  {
+    link: '/questions',
+    icon: <GlobalIcon className='icon' />,
+    text: 'UCET Community',
+  },
+  {
+    link: '/tags',
+    text: 'Tags',
+  },
+  {
+    link: '/users',
+    text: 'Users',
+  }
+]
