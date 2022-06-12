6c266ee9d5ef5ead14f0d2b0587ef5b205358ae1
bhartiomee <bhartiomee25@gmail.com>
2022-06-12 18:15:31 +0530

    Initail Setup

---
+++ server/src/services/posts.service.js
@@ -0,0 +1,16 @@
+const { postsRepository } = require('../repositories');
+
+exports.create = (newPost, result) => {
+  postsRepository.create(newPost, result);
+};
+
+exports.remove = (id, result) => postsRepository.remove(id, result);
+
+exports.retrieveOne = (postId, result) => postsRepository.retrieveOne(postId, result);
+
+exports.retrieveAll = (result) => postsRepository.retrieveAll(result);
+
+exports.retrieveAllTag = (tagName, result) => postsRepository.retrieveAllTag(tagName, result);
+
+exports.retrieveAllQuestions=(category,result)=>postsRepository.retrieveAllCategory(category,result);
+
